9cb332e87ab56c69c17bf6651c348f48e135b59e
Staff <staff@staff-2.local>
2016-03-28 09:15:19 -0700

    Add notes

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -2,3 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 });
+//component is two parts: handlebars template that is the display and a js source file defining behavior.
+//components must have a dash. rental not ok. rental-tile is ok.
+//we generate a new route when it's a new url. a component is a piece of the functionality on one part of the page. can put it on any url by instantiating it in a template.
